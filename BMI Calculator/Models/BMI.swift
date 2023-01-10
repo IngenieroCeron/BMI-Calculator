ad733dceb5b1e1d77bfcb2872b78baf588f19184
Eduardo Ceron <ing.ceron@192.168.5.31>
2023-01-10 06:39:38 -0600

    Commit Inicial

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Eduardo Ceron on 21/08/2021.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
